55bff59ffbb3104a0864a8bd615e1a6ebe9a62ea
Cemal <cml_gndz@hotmail.com>
2020-05-19 22:10:59 +0300

    Prosumer ve Supplier layoutları tamamlandı.

---
+++ app/src/main/java/com/grapesoftware/paycal_resist/StorageTypeActivity.java
@@ -0,0 +1,14 @@
+package com.grapesoftware.paycal_resist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StorageTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage_type);
+    }
+}

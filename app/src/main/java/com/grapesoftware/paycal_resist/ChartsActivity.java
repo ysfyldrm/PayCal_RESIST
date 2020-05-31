4ea72454f74fbdf86dd6fe130c4d686bbd7110ef
Cemal <cml_gndz@hotmail.com>
2020-05-31 22:52:11 +0300

    UI improvements

--- app/src/main/java/com/grapesoftware/paycal_resist/ChartsActivity.java
+++ app/src/main/java/com/grapesoftware/paycal_resist/ChartsActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -53,7 +54,7 @@ public class ChartsActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LinearLayout savingLayout = (LinearLayout) findViewById(R.id.linearLayout_chart);
+                RelativeLayout savingLayout = (RelativeLayout) findViewById(R.id.save_layout);
                 File file = saveBitMap(ChartsActivity.this, savingLayout);
                 if (file != null) {
                     Log.i("TAG", "Drawing saved to the gallery!");
